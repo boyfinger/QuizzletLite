65c141b35aba8f144a26287070c03c5bf2c3f9ab
boyfinger <122414066+boyfinger@users.noreply.github.com>
2025-06-17 09:20:51 +0700

    new dbcontext

--- API/DAO/QuizletLiteContext.cs
+++ API/DAO/QuizletLiteContext.cs
@@ -1,74 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models.Enums;
+using API.Models.Snapshots;
+using Newtonsoft.Json;
 using API.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.DAO;
 
 public partial class QuizletLiteContext : DbContext
 {
+    public QuizletLiteContext()
+    {
+    }
+
     public QuizletLiteContext(DbContextOptions<QuizletLiteContext> options)
         : base(options)
     {
     }
 
-    public virtual DbSet<Answer> Answers { get; set; }
-
     public virtual DbSet<Question> Questions { get; set; }
-
-    public virtual DbSet<QuestionType> QuestionTypes { get; set; }
 
     public virtual DbSet<Quiz> Quizzes { get; set; }
 
-    public virtual DbSet<QuizResult> QuizResults { get; set; }
-
-    public virtual DbSet<Role> Roles { get; set; }
-
-    public virtual DbSet<SelectedAnswer> SelectedAnswers { get; set; }
+    public virtual DbSet<QuizAttempt> QuizAttempts { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<SelectedAnswer>()
-            .HasOne(sa => sa.Question)
-            .WithMany(q => q.SelectedAnswers)
-            .HasForeignKey(sa => sa.QuestionId)
+        modelBuilder.Entity<QuizAttempt>()
+            .HasOne(qa => qa.User)
+            .WithMany(u => u.QuizAttempts)
+            .HasForeignKey(qa => qa.UserId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<SelectedAnswer>()
-           .HasOne(sa => sa.QuizResult)
-           .WithMany(qr => qr.SelectedAnswers)
-           .HasForeignKey(sa => sa.QuizResultId)
-           .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<SelectedAnswer>()
-            .HasOne(sa => sa.Answer)
-            .WithMany(a => a.SelectedAnswers)
-            .HasForeignKey(sa => sa.AnswerId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Quiz>().
-            HasOne(q => q.CreatedByNavigation)
+        modelBuilder.Entity<Quiz>()
+            .HasOne(q => q.CreatedByNavigation)
             .WithMany(u => u.Quizzes)
             .HasForeignKey(q => q.CreatedBy)
             .OnDelete(DeleteBehavior.Restrict);
 
-
-        modelBuilder.Entity<Role>().HasData(
-            new Role { Id = 1, Role1 = "Admin" },
-            new Role { Id = 2, Role1 = "User" }
-        );
-
         modelBuilder.Entity<User>().HasData(//AnLT Đổi Tí Mật Khẩu, Mật Khẩu của chúng ta sẽ là mã hóa Bcrypt
-            new User { Id = 1, RoleId = 1, Email = "admin@email.com", Username = "admin", Password = "$2a$12$6GMyaacdt22VmPBouyUnB.e/4guoGG09ukoXkx/eb02bKcwokoy9C", Avatar = null },
-            new User { Id = 2, RoleId = 2, Email = "user1@email.com", Username = "user1", Password = "$2a$12$6GMyaacdt22VmPBouyUnB.e/4guoGG09ukoXkx/eb02bKcwokoy9C", Avatar = null },
-            new User { Id = 3, RoleId = 2, Email = "user2@email.com", Username = "user2", Password = "$2a$12$6GMyaacdt22VmPBouyUnB.e/4guoGG09ukoXkx/eb02bKcwokoy9C", Avatar = null }
-        );
-
-        modelBuilder.Entity<QuestionType>().HasData(
-            new QuestionType { Id = 1, Name = "Single Choice" },
-            new QuestionType { Id = 2, Name = "Multiple Choice" }
+            new User { Id = 1, Role = Role.Admin, Email = "admin@email.com", Username = "admin", PasswordHash = "$2a$12$6GMyaacdt22VmPBouyUnB.e/4guoGG09ukoXkx/eb02bKcwokoy9C", Avatar = null },
+            new User { Id = 2, Role = Role.User, Email = "user1@email.com", Username = "user1", PasswordHash = "$2a$12$6GMyaacdt22VmPBouyUnB.e/4guoGG09ukoXkx/eb02bKcwokoy9C", Avatar = null },
+            new User { Id = 3, Role = Role.User, Email = "user2@email.com", Username = "user2", PasswordHash = "$2a$12$6GMyaacdt22VmPBouyUnB.e/4guoGG09ukoXkx/eb02bKcwokoy9C", Avatar = null }
         );
 
         modelBuilder.Entity<Quiz>().HasData(
@@ -77,20 +51,82 @@ public partial class QuizletLiteContext : DbContext
             new Quiz { Id = 3, Name = "History Challenge", CreatedBy = 3, CreatedOn = DateTime.Now, IsActive = true }
         );
 
-        modelBuilder.Entity<Question>().HasData(
-            new Question { Id = 1, Content = "What is the capital of France?", QuizId = 1, QuestionTypeId = 1 },
-            new Question { Id = 2, Content = "Which planets are part of the solar system?", QuizId = 2, QuestionTypeId = 2 },
-            new Question { Id = 3, Content = "Who was the first President of the United States?", QuizId = 3, QuestionTypeId = 1 }
-        );
+        var question1Answers = new List<QuestionOptionsSnapshot>
+        {
+            new QuestionOptionsSnapshot { Content = "Paris", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "London", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Berlin", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Madrid", IsCorrect = false }
+        };
+        var question2Answers = new List<QuestionOptionsSnapshot>
+        {
+            new QuestionOptionsSnapshot { Content = "Mercury", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "Venus", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "Earth", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "Pluto", IsCorrect = false }
+        };
+        var question3Answers = new List<QuestionOptionsSnapshot>
+        {
+            new QuestionOptionsSnapshot { Content = "George Washington", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "Thomas Jefferson", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Abraham Lincoln", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "John Adams", IsCorrect = false }
+        };
+        var question4Answers = new List<QuestionOptionsSnapshot>
+        {
+            new QuestionOptionsSnapshot { Content = "Hanoi", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "Ho Chi Minh City", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Da Nang", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Hue", IsCorrect = false }
+        };
+        var question5Answers = new List<QuestionOptionsSnapshot>
+        {
+            new QuestionOptionsSnapshot { Content = "Jupiter", IsCorrect = true },
+            new QuestionOptionsSnapshot { Content = "Saturn", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Earth", IsCorrect = false },
+            new QuestionOptionsSnapshot { Content = "Mars", IsCorrect = false }
+        };
 
-        modelBuilder.Entity<Answer>().HasData(
-            new Answer { Id = 1, Content = "Paris", QuestionId = 1, IsCorrect = true },
-            new Answer { Id = 2, Content = "London", QuestionId = 1, IsCorrect = false },
-            new Answer { Id = 3, Content = "Mars", QuestionId = 2, IsCorrect = true },
-            new Answer { Id = 4, Content = "Venus", QuestionId = 2, IsCorrect = true },
-            new Answer { Id = 5, Content = "Pluto", QuestionId = 2, IsCorrect = false },
-            new Answer { Id = 6, Content = "George Washington", QuestionId = 3, IsCorrect = true },
-            new Answer { Id = 7, Content = "Thomas Jefferson", QuestionId = 3, IsCorrect = false }
-        );
+        modelBuilder.Entity<Question>().HasData(
+            new Question
+            {
+                Id = 1,
+                Content = "What is the capital of France?",
+                QuizId = 1,
+                QuestionType = QuestionType.SingleChoice,
+                OptionsJson = JsonConvert.SerializeObject(question1Answers)
+            },
+            new Question
+            {
+                Id = 2,
+                Content = "Which planets are part of the solar system?",
+                QuizId = 2,
+                QuestionType = QuestionType.MultipleChoice,
+                OptionsJson = JsonConvert.SerializeObject(question2Answers)
+            },
+            new Question
+            {
+                Id = 3,
+                Content = "Who was the first President of the United States?",
+                QuizId = 3,
+                QuestionType = QuestionType.SingleChoice,
+                OptionsJson = JsonConvert.SerializeObject(question3Answers)
+            },
+            new Question
+            {
+                Id = 4,
+                Content = "What is the capital of Vietnam?",
+                QuizId = 1,
+                QuestionType = QuestionType.SingleChoice,
+                OptionsJson = JsonConvert.SerializeObject(question4Answers)
+            },
+            new Question
+            {
+                Id = 5,
+                Content = "What is the largest planet in our solar system?",
+                QuizId = 2,
+                QuestionType = QuestionType.SingleChoice,
+                OptionsJson = JsonConvert.SerializeObject(question5Answers)
+            });
     }
 }
