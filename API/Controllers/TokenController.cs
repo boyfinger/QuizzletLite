b2d24379cdbfed96c03ef6b75cf3c240e4495717
boyfinger <122414066+boyfinger@users.noreply.github.com>
2025-06-21 15:07:08 +0700

    inject jwt & odata

---
+++ API/Controllers/TokenController.cs
@@ -0,0 +1,24 @@
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public IActionResult GenerateToken(int userId, string role)
+        {
+            return Ok(new { token = JWTHelper.GenerateToken(userId.ToString(), role, _configuration) });
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,15 @@
-using API.DAO;
+﻿using API.DAO;
+using API.Models;
 using API.Repositories;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,23 +30,86 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "QuizzletLite API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 builder.Services.AddScoped<IQuizRepository, QuizRepository>();
 builder.Services.AddScoped<IQuizService, QuizService>();
-builder.Services.AddScoped<IQuizResultService, QuizResultService>();
-builder.Services.AddScoped<IQuizResultRepository, QuizResultRepository>();
+builder.Services.AddScoped<IQuizAttemptService, QuizAttemptService>();
+builder.Services.AddScoped<IQuizAttemptRepository, QuizAttemptRepository>();
 
 builder.Services.AddScoped<Admin_IQuizRepository, Admin_QuizRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<Admin_IQuizService, Admin_QuizService>();
 
-builder.Services.AddScoped<IJsonConverterService, JsonConverterService>();
+builder.Services.AddControllers().AddOData(options => options
+    .Filter()
+    .OrderBy()
+    .Select()
+    .Count()
+    .Expand()
+    .SetMaxTop(100)
+    .AddRouteComponents("odata", getEdmModel())
+);
+IEdmModel getEdmModel()
+{
+    var builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Question>("Questions");
+    builder.EntitySet<Quiz>("Quizzes");
+    builder.EntitySet<QuizAttempt>("QuizAttempts");
+    builder.EntitySet<User>("Users");
+    return builder.GetEdmModel();
+}
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+    opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
 
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"]))
 
+        };
+    });
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+});
 
 var app = builder.Build();
 
