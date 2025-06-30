322f624a1d51d7d14b4a8cddc7fba41d07f6e981
dungle2107 <ttd21072004@gmail.com>
2025-07-01 06:04:15 +0700

    update

--- API/Dtos/User/UserDto.cs
+++ API/Dtos/User/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using API.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using API.Models.Enums;
 
 namespace API.Dtos.User
 {
@@ -6,11 +7,17 @@ namespace API.Dtos.User
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Role is required.")]
         public Role Role { get; set; }
+
         public string? Avatar { get; set; }
 
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Username must be at least 3 characters.")]
         public string Username { get; set; } = null!;
 
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = null!;
     }
 }
