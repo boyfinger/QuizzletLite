ffbf5d594bb93546322750038cfce857120fa884
boyfinger <122414066+boyfinger@users.noreply.github.com>
2025-07-26 10:27:34 +0700

    saving answers progress

--- WebApp/Pages/Quiz/QuizTaking.cshtml.cs
+++ WebApp/Pages/Quiz/QuizTaking.cshtml.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Net;
 using WebApp.Helpers;
+using API.Dtos.User;
 
 namespace WebApp.Pages.Quiz
 {
@@ -13,16 +14,21 @@ namespace WebApp.Pages.Quiz
         private readonly HttpClient _httpClient;
         private readonly string _baseUrl;
 
-        [BindProperty(SupportsGet = true)]
-        public int Id { get; set; }
-        [BindProperty]
-        public QuizDetailsDto QuizDetails { get; set; }
-
         public QuizTakingModel(HttpClient httpClient)
         {
             _httpClient = httpClient;
             _baseUrl = APIUrlHelper.GetBaseUrl();
         }
+
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
+        [BindProperty]
+        public QuizDetailsDto QuizDetails { get; set; }
+
+        [BindProperty]
+        public UserDto? UserInfo { get; set; }
+
         public async Task<IActionResult> OnGet()
         {
             var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/quiz/{Id}/questions");
@@ -46,6 +52,12 @@ namespace WebApp.Pages.Quiz
             {
                 var jsonString = await response.Content.ReadAsStringAsync();
                 QuizDetails = JsonConvert.DeserializeObject<QuizDetailsDto>(jsonString) ?? new QuizDetailsDto();
+
+                request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/api/authentication/me");
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                response = await _httpClient.SendAsync(request);
+                UserInfo = JsonConvert.DeserializeObject<UserDto>(await response.Content.ReadAsStringAsync());
+
                 return Page();
             }
         }
