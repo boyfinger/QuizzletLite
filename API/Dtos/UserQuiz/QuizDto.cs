99ea345ec4260520a13d9e2fd2ed0837e63db7ab
boyfinger <122414066+boyfinger@users.noreply.github.com>
2025-07-26 13:21:12 +0700

    Reapply "crud user quiz"
    This reverts commit 3315b08233cf7c24b892490afa62df439cdfe5d3.

---
+++ API/Dtos/UserQuiz/QuizDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Dtos.UserQuiz
+{
+    public class QuizDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+}
---
+++ API/Services/UserQuizService.cs
@@ -0,0 +1,48 @@
+﻿using API.Dtos.UserQuiz;
+using API.Repositories;
+using API.Models;
+
+namespace API.Services
+{
+    public class UserQuizService : IUserQuizService
+    {
+        private readonly IUserQuizRepository _repo;
+
+        public UserQuizService(IUserQuizRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<int> CreateUserQuiz(UserQuizDto dto, int userId)
+        {
+            return await _repo.CreateUserQuiz(dto, userId);
+        }
+
+        public async Task<bool> UpdateUserQuiz(UserQuizDto dto, int userId)
+        {
+            return await _repo.UpdateUserQuiz(dto, userId);
+        }
+
+        public async Task<bool> DeleteUserQuiz(int quizId, int userId)
+        {
+            return await _repo.DeleteUserQuiz(quizId, userId);
+        }
+
+        public async Task<Quiz?> GetUserQuizDetail(int quizId, int userId)
+        {
+            return await _repo.GetQuizById(quizId, userId);
+        }
+        public async Task<IEnumerable<QuizDto>> GetUserQuizzes(int userId)
+        {
+            return await _repo.GetUserQuizzesAsync(userId);
+        }
+        public async Task<bool> DeleteQuestion(int quizId, int questionId, int userId)
+        {
+            return await _repo.DeleteQuestionAsync(quizId, questionId);
+        }
+        public async Task<bool> UpdateActiveStatusAsync(int quizId, bool isActive)
+        {
+            return await _repo.UpdateActiveStatusAsync(quizId, isActive);
+        }
+    }
+}
