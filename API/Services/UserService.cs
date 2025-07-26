44c7cbd8efa2dd9cfc8e6834ade49a6b346ab6c2
LeThaiAn15112004 <anlthe181137@fpt.edu.vn>
2025-07-26 13:22:46 +0700

    Update UserService.cs

--- API/Services/UserService.cs
+++ API/Services/UserService.cs
@@ -1,87 +1,88 @@
-﻿    using API.Dtos.User;
-    using API.Helpers;
-    using API.Mappers;
-    using API.Repositories;
+﻿using API.Dtos.User;
+using API.Helpers;
+using API.Mappers;
+using API.Repositories;
 
-    namespace API.Services
+namespace API.Services
+{
+    public class UserService : IUserService
     {
-        public class UserService : IUserService
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
         {
-            private readonly IUserRepository _userRepository;
-
-            public UserService(IUserRepository userRepository)
-            {
-                _userRepository = userRepository;
-            }
-
-            public async Task<PagedResult<UserDto>> GetUsers(UserQuery query)
-            {
-                var pagedUsers = await _userRepository.GetUsers(query);
-                var userDtos = pagedUsers.Items.Select(u => u.ToUserDto()).ToList();
-
-                return new PagedResult<UserDto>(userDtos, pagedUsers.TotalCount, pagedUsers.Page, pagedUsers.PageSize);
-            }
-
-            public async Task<UserDto?> GetUserById(int id)
-            {
-                var user = await _userRepository.GetUserById(id);
-                return user?.ToUserDto();
-            }
-
-            public async Task AddUser(UserDto userDto)
-            {
-                // Ví dụ bạn check tồn tại theo username hoặc email
-                var existingUser = await _userRepository.GetUserByEmail(userDto.Email);
-                if (existingUser != null)
-                {
-                    throw new Exception("User with this email already exists.");
-                }
-                // Nếu muốn check username riêng thì cũng thêm tương tự
-
-                var user = userDto.ToUser();
-                await _userRepository.AddUser(user);
-            }
-
-            public async Task UpdateUser(UserDto userDto)
-            {
-                if (!await _userRepository.UserExists(userDto.Id))
-                {
-                    throw new Exception($"User with ID {userDto.Id} does not exist.");
-                }
-                await _userRepository.UpdateUser(userDto);
-            }
-
-            public async Task<bool> DeleteUser(int id)
-            {
-                if (!await _userRepository.UserExists(id))
-                {
-                    return false;
-                }
-
-                return await _userRepository.DeleteUser(id);
-            }
-
-            public async Task<bool> UserExists(int id)
-            {
-                return await _userRepository.UserExists(id);
-            }
-
-            public async Task<string?> GetAvatarByUserId(int userId)
-            {
-                return await _userRepository.GetAvatarByUserId(userId);
-            }
+            _userRepository = userRepository;
         }
-        public IQueryable<UserDto> GetUsersForOData()
-    {
-        return _userRepository.GetUsersQueryable()
-            .Select(u => new UserDto
+
+        public async Task<PagedResult<UserDto>> GetUsers(UserQuery query)
+        {
+            var pagedUsers = await _userRepository.GetUsers(query);
+            var userDtos = pagedUsers.Items.Select(u => u.ToUserDto()).ToList();
+
+            return new PagedResult<UserDto>(userDtos, pagedUsers.TotalCount, pagedUsers.Page, pagedUsers.PageSize);
+        }
+
+        public async Task<UserDto?> GetUserById(int id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            return user?.ToUserDto();
+        }
+
+        public async Task AddUser(UserDto userDto)
+        {
+            // Ví dụ bạn check tồn tại theo username hoặc email
+            var existingUser = await _userRepository.GetUserByEmail(userDto.Email);
+            if (existingUser != null)
             {
-                Id = u.Id,
-                Avatar = u.Avatar,
-                Username = u.Username,
-                Email = u.Email,
-                Role = u.Role
-            });
-    }
+                throw new Exception("User with this email already exists.");
+            }
+            // Nếu muốn check username riêng thì cũng thêm tương tự
+
+            var user = userDto.ToUser();
+            await _userRepository.AddUser(user);
+        }
+
+        public async Task UpdateUser(UserDto userDto)
+        {
+            if (!await _userRepository.UserExists(userDto.Id))
+            {
+                throw new Exception($"User with ID {userDto.Id} does not exist.");
+            }
+            await _userRepository.UpdateUser(userDto);
+        }
+
+        public async Task<bool> DeleteUser(int id)
+        {
+            if (!await _userRepository.UserExists(id))
+            {
+                return false;
+            }
+
+            return await _userRepository.DeleteUser(id);
+        }
+
+        public async Task<bool> UserExists(int id)
+        {
+            return await _userRepository.UserExists(id);
+        }
+
+        public async Task<string?> GetAvatarByUserId(int userId)
+        {
+            return await _userRepository.GetAvatarByUserId(userId);
+        }
+
+        public IQueryable<UserDto> GetUsersForOData()
+        {
+            return _userRepository.GetUsersQueryable()
+                .Select(u => new UserDto
+                {
+                    Id = u.Id,
+                    Avatar = u.Avatar,
+                    Username = u.Username,
+                    Email = u.Email,
+                    Role = u.Role
+                });
+        }
 
     }
+}
